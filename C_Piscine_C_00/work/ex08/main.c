5146e1658f62bdce3cb10d97f52a75bf547b89ac
SCelis <31612724+SCelisV@users.noreply.github.com>
2022-08-20 19:06:07 +0200

    scelis - work - examples videos

--- C_Piscine_C_00/work/ex08/main.c
+++ C_Piscine_C_00/work/ex08/main.c
@@ -13,7 +13,7 @@ int main(void)
 	a[5][0] = 5;
 	a[3][b] = a[0][b];
 	//a[10] = 10; 			// *** stack smashing detected ***: terminated
-					// Aborted (core dumped)
+							// Aborted (core dumped)
 
 
 	for (int r=0; r < 10 ; r++)
