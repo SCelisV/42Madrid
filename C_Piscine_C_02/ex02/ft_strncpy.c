cef170342fb4f7cd2a1e5a4981b215d031142dbe
SCelis <31612724+SCelisV@users.noreply.github.com>
2022-08-29 21:26:33 +0200

    scelis - C_02_ v01

---
+++ C_Piscine_C_02/ex02/ft_strncpy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned	int	i;
+	i = 0;
+	while(src[i] != '\0' && i <= n) 
+	{
+		dest[i] = src[i];
+		printf("src[%d]: %d \n", i, src[i]);
+		printf("dest[%d]: %d \n", i, dest[i]);
+		i++;
+	}
+	return(dest);
+}
