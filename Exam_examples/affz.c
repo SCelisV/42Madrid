db0b4e0ef2be5394bf6d83a97e145f7e236b0218
SCelis <31612724+SCelisV@users.noreply.github.com>
2022-08-29 17:39:04 +0200

    in progress

---
+++ Exam_examples/affz.c
@@ -0,0 +1,17 @@
+El enunciado es algo parecido a:
+Hacer un programa que reciba un argumento cadena de caracteres; 
+buscar la primera "z" que encuentre y escribir "z" y un cambio de linea
+si no hay z, escribir "z" y un cambio de linea
+si hay más de un argumento escriba "z" y un cambio de linea
+funciones permititadas write
+
+
+Respuesta: esta chorrada la hacen para ver si tu "piensas en una solución fácil" y no hacer un arco de iglesia... 
+
+alguien me "explico",,, que lo hacian para que no pasara lo mismo que los rusos con el lápiz lo cual.. no es totalmente cierto pero en fin.. 
+me pareció una engañifa sin más ni más... 
+
+https://verne.elpais.com/verne/2016/04/15/articulo/1460714159_779370.html
+
+porque lo único que quieren que hagas es escribir una línea de código simple: write(1, "z\n", 2);
+
